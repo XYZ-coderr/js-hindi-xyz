6f0159b41789d2313156b7f309f30ce55fddc912
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-19 18:33:06 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"//ek variable hh 
+let zz = "Numan"
+console.log(typeof score);
+console.log(typeof (score)); // yeh method tareeka hh likhne ka 
+
+let valueNumber = Number(score) // converted the string in number
+console.log(typeof valueNumber);
+console.log( valueNumber);
+let valuennn = Number(zz) // converted the string in number ; but it dose not converted as it is in string
+console.log(valuennn);
+
+let isLoggedIn = 1
+let booleanISLOggedIn = Boolean(isLoggedIn) // convertd in boolean fromm number
+console.log(booleanISLOggedIn);
+
+// 1 likhne pr true agr zero toh false dega 
+// Numan likhne pr woh true dera or khaali rkhne se false dera 
+
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber); // string m covert ho chuka h pr 33 number form m hi print huwa hh 
