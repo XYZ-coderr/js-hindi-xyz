b5d5a5342bbc4f638d93dc00cd676232fa386885
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-20 18:18:45 +0000

    Data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// PRIMITIVE DATA TYPES
+
+// 7 types : (Call by values )String , Number , Boolean , null , undefined , symbol , BigInt
+
+
+// REFERNCE TYPE YA NON PRIMITIVE DATA TYPES
+
+// Array , objects , functions 
+let muObj = // tareeka hh object likhne ka
+{
+    name : "Numan",
+    age : 20,
+}
+
+const myFunction = function(){    // function likhne ka tareeka 
+
+    console.log("Hello world");
+    
+
+}
+console.log(typeof myFunction);
