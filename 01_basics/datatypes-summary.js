3894d6d2d1ad5d418ce1be4a1082ac05e43b7900
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-20 18:43:23 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -19,3 +19,29 @@ const myFunction = function(){    // function likhne ka tareeka
 
 }
 console.log(typeof myFunction);
+
+
+
+
+//*********************************** */
+//MEMORY 
+//STACK MEMORY (PRIMITIVE M AATA HH ) = ISKA MTLB JO BHI VARIABLE DECLARE KIYA USKA COPY MILTA HH 
+// HEAP MEMORY (NON PRIMITIVE) = ISKA MTLB JO BHI VARIABLE DECLARE KIYA USKA REFRENCE  MILTA HH
+let myname = "Numan"
+
+
+let anothername = myname
+anothername = "Raza"
+
+console.log(myname);
+console.log(anothername);
+
+let user1 ={
+    email: "raza@gmail.com",
+    upi:"usser@123",
+}
+let user2 = user1
+
+user2.email ="numan@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
