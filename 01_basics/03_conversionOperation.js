b53856a4d57cbb7e5256854e0ba91101d068e692
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-20 17:51:08 +0000

    Comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+// ****************************** Conversion ***********************
 let score = "33"//ek variable hh 
 let zz = "Numan"
 console.log(typeof score);
@@ -21,3 +22,26 @@ let someNumber = 33
 let StringNumber = String(someNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber); // string m covert ho chuka h pr 33 number form m hi print huwa hh 
+
+//******************* operations ************** */
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2**3); // 2^3
+
+let str1 ="Numan"
+let str2 = "Raza"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 +2); // string phle aaya isliye string form m saara values utaar diya 
+console.log( 1 + 2 +"2"); // number phle aaya isliye phle number form me rkha add kiya phir string k sth joda 
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+//******************COMPARISION **************************/
+console.log(2>1);
+console.log("2">1);// automatically java script ne string 2 ko numbers m convert krdiya kyunki javascript do alg data type ko compare nhi krne deta hh 
+console.log(null > 0 );
+console.log(null == 0);
+console.log(null >= 0);
+  
+
+// === strict check , sth m data type bhi check krta hh 
+console.log(2 === 2);
+console.log("2" === 2); // sth m data type bhi check krta hh 
