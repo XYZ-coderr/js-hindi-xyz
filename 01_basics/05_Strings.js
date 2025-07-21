b6f9b1cce7e6af9ce1256ea41ba89df8af5dbdf0
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-21 15:23:14 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,15 @@
+const name = "numan"
+const repoCount = 50
+// console.log(name   +   repoCount); //old versionn of adding strings
+
+console.log(`Hello my name is ${name} and my repository count is ${repoCount} and upper case of name is ${name.toUpperCase()}`);
+
+console.log(name.charAt(2));
+console.log(name.indexOf('n'));
+console.log(name.substring(0,3));
+ 
+
+const newString = "numan-raza-ziyai"
+console.log(newString.split('-')); // split the string in array form 
+
+
