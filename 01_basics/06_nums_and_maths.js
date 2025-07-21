2b13f1e828bf95873a0139fc158afeea67d8c368
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-21 15:45:03 +0000

    sums and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+//***************NUMS******************* */\
+const score = 400
+console.log(score);
+
+
+const balance = new Number(100) // coonnfirms that the number is in integer
+console.log(balance);
+ 
+console.log(balance.toString().length); // now the number is converted to string 
+console.log(balance.toFixed(2)); // adds decimal points 
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); //adds commas
+
+// **************MAthS***************
+
+console.log(Math.abs(-4)); // prints in positive
+
