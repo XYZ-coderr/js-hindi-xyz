8e7c0640136e405b67ce56419b2e7cde5993e0ad
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-29 15:34:42 +0000

    control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -7,3 +7,15 @@ else{
     console.log("not found");
     
 }
+// Nullish coalescing operator (??) : null and undefined 
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10
+
+console.log(val1);
+
+// Terniary Operator 
+//condition ? true : false
+const iceTea =  100
+iceTea <= 80 ? console.log("less than 80") : console.log("more than 80");
+
