236d3d1d115f32d99629c610c04c097aa0aab03e
XYZ-coderr <ns2021gameid@gmail.com>
2025-07-29 17:52:12 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,19 @@
+// for of loop 
+
+const arr = [ 1 , 2 , 3 , 4 , 5]
+for (const num  of arr) {
+    console.log(num);
+    
+}
+
+
+const greetings = "HELLO WORLD"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+    
+}
+
+
+//Maps
+
+const map = new Map()   // object 
